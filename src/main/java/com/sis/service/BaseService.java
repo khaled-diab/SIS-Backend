80cebe1d36ffe4ac3dbd9c6498c1da4fadbb7656
Moustafa Mohamed <M0D4@GitHub.com>
2021-10-25 00:26:30 +0200

    Added "filterBy" function

--- src/main/java/com/sis/service/BaseService.java
+++ src/main/java/com/sis/service/BaseService.java
@@ -1,6 +1,8 @@
 package com.sis.service;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sis.entities.BaseEntity;
@@ -8,9 +10,9 @@ import com.sis.util.PageQueryUtil;
 import com.sis.util.PageResult;
 import org.springframework.data.util.Pair;
 
-public interface BaseService<E extends BaseEntity > {
-	
-	public List<E> findAll();
+public interface BaseService<E extends BaseEntity> {
+
+    public List<E> findAll();
 
     public List<E> find(String key);
 
@@ -20,13 +22,13 @@ public interface BaseService<E extends BaseEntity > {
 
     public E save(E entity);
 
-	public void delete(E entity);
-	
-	public void deleteById(Long id);
-	
-	public PageResult<E> getDataPage(PageQueryUtil pageUtil);
-	
-	public PageResult<E> getDataPage(PageQueryUtil pageUtil, String sortField , Direction sortDirection);
-	
-	public JpaRepository<E, Long> Repository();
+    public void delete(E entity);
+
+    public void deleteById(Long id);
+
+    public PageResult<E> getDataPage(PageQueryUtil pageUtil);
+
+    public PageResult<E> getDataPage(PageQueryUtil pageUtil, String sortField, Direction sortDirection);
+
+    public JpaRepository<E, Long> Repository();
 }
--- src/main/java/com/sis/service/BaseServiceImp.java
+++ src/main/java/com/sis/service/BaseServiceImp.java
@@ -3,6 +3,7 @@ package com.sis.service;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,70 +20,92 @@ import com.sis.util.PageResult;
 
 
 public abstract class BaseServiceImp<E extends BaseEntity> implements BaseService<E> {
-	@Override
-	public List<E> findAll() {
-		List<E> entities = Repository().findAll();
-		return entities;
-	}
+    @Override
+    public List<E> findAll() {
+        List<E> entities = Repository().findAll();
+        return entities;
+    }
 
-	@Override
-	public E findById(Long id) {		
-		return Repository().findById(id).orElseThrow(() -> new ItemNotFoundException(id));	
-	}
+    @Override
+    public E findById(Long id) {
+        return Repository().findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+    }
 
-	@Override
-	@Transactional(readOnly = false)
-	public E save(E entity) {
-		return Repository().save(entity);
-	}
+    @Override
+    @Transactional(readOnly = false)
+    public E save(E entity) {
+        return Repository().save(entity);
+    }
 
-	@Override
-	@Transactional(readOnly = false)
-	public void delete(E entity) {
-		Repository().delete(entity);
-	}
+    @Override
+    @Transactional(readOnly = false)
+    public void delete(E entity) {
+        Repository().delete(entity);
+    }
 
-	@Override
-	public void deleteById(Long id) {
-		Repository().deleteById(id);
-	}
+    @Override
+    public void deleteById(Long id) {
+        Repository().deleteById(id);
+    }
 
-	@Override
-	public PageResult<E> getDataPage(PageQueryUtil pageUtil) {
-		PageResult<E> pageResult = getDataPage(pageUtil);
-		return pageResult;
-	}
+    @Override
+    public PageResult<E> getDataPage(PageQueryUtil pageUtil) {
+        PageResult<E> pageResult = getDataPage(pageUtil);
+        return pageResult;
+    }
 
-	@Override
-	public PageResult<E> getDataPage(PageQueryUtil pageUtil, @Nullable String sortField,@Nullable Direction sortDirection) {
-		Sort sort = sortDirection.equals(Sort.Direction.ASC) ? Sort.by(sortField).ascending()
-				: Sort.by(sortField).descending();
-		Pageable pageable = PageRequest.of(pageUtil.getPage() - 1, pageUtil.getLimit(), sort);
-		Page<E> page = this.Repository().findAll(pageable);
-		PageResult<E> pageResult = new PageResult<E>(page.getContent(), (int) page.getTotalElements(),
-				pageUtil.getLimit(), pageUtil.getPage());
-		return pageResult;
-	}
+    @Override
+    public PageResult<E> getDataPage(PageQueryUtil pageUtil, @Nullable String sortField, @Nullable Direction sortDirection) {
+        Sort sort = sortDirection.equals(Sort.Direction.ASC) ? Sort.by(sortField).ascending()
+                : Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageUtil.getPage() - 1, pageUtil.getLimit(), sort);
+        Page<E> page = this.Repository().findAll(pageable);
+        PageResult<E> pageResult = new PageResult<E>(page.getContent(), (int) page.getTotalElements(),
+                pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
 
-	@Override
-	public List<E> find(String key){
-		key = key.toLowerCase();
-		String finalKey = key;
-		List<E> list = Repository().findAll().stream().filter(e -> {
-			Field[] declaredFields = e.getClass().getDeclaredFields();
-			boolean found = e.getId().toString().toLowerCase().contains(finalKey);
-			for (Field declaredField : declaredFields) {
-				try {
-					declaredField.setAccessible(true);
-					Object value = declaredField.get(e);
-					if (value == null) continue;
-					found |= value.toString().toLowerCase().contains(finalKey.toLowerCase());
-				} catch (IllegalAccessException | NullPointerException exception) {
-					exception.printStackTrace();
-				}
-			}
-			return found;
-		}).collect(Collectors.toList());
-		return list;
-	}
+    @Override
+    public List<E> find(String key) {
+        key = key.toLowerCase();
+        String finalKey = key;
+        List<E> list = Repository().findAll().stream().filter(e -> {
+            Field[] declaredFields = e.getClass().getDeclaredFields();
+            boolean found = e.getId().toString().toLowerCase().contains(finalKey);
+            for (Field declaredField : declaredFields) {
+                try {
+                    declaredField.setAccessible(true);
+                    Object value = declaredField.get(e);
+                    if (value == null) continue;
+                    found |= value.toString().toLowerCase().contains(finalKey.toLowerCase());
+                } catch (IllegalAccessException | NullPointerException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            return found;
+        }).collect(Collectors.toList());
+        return list;
+    }
+
+    @Override
+    public List<E> filterBy(Map<String, String> whereClause) {
+        List<E> res = Repository().findAll().stream().filter(e -> {
+            boolean found = false;
+            for(Map.Entry<String, String> keys : whereClause.entrySet()){
+                String[] values = keys.getValue().split("\\+");
+                try {
+                    Field field = e.getClass().getDeclaredField(keys.getKey());
+                    field.setAccessible(true);
+                    Object ob = field.get(e);
+                    for (String value : values) {
+                        found |= ob.toString().toLowerCase().contains(value.toLowerCase());
+                    }
+                } catch (NoSuchFieldException | IllegalAccessException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return found;
+        }).collect(Collectors.toList());
+        return res;
+    }
 }
