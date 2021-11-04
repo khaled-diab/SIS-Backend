557b9e6e453c4701199ffc914994bedb58f3ce6e
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2021-11-04 23:34:07 +0200

    faculty member merge

--- src/main/java/com/sis/controller/FacultyMemberController.java
+++ src/main/java/com/sis/controller/FacultyMemberController.java
@@ -1,32 +1,24 @@
 package com.sis.controller;
 
+import com.sis.dto.FacultyMemberDTO;
+import com.sis.entities.FacultyMember;
 import com.sis.entities.mapper.FacultyMemberMapper;
-import com.sis.service.BaseServiceImp;
 import com.sis.service.FacultyMemberService;
 import com.sis.util.MessageResponse;
 import com.sis.util.PageQueryUtil;
 import com.sis.util.PageResult;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.sis.dto.FacultyMemberDTO;
-import com.sis.entities.FacultyMember;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/facultyMember")
-public class FacultyMemberController extends BaseController<FacultyMember, FacultyMemberDTO>{
+@AllArgsConstructor
+public class FacultyMemberController extends BaseController<FacultyMember, FacultyMemberDTO> {
 
-    BaseServiceImp baseServiceImp;
 
-    @Autowired
-    FacultyMemberService facultyMemberService;
-
-    @Autowired
-    FacultyMemberMapper facultyMemberMapper;
+    private final FacultyMemberService facultyMemberService;
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
     public MessageResponse up(@RequestBody FacultyMemberDTO dto) {
