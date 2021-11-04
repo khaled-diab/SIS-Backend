597afe89d1cedc3b82e9993638a8328bc0e53a06
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2021-11-04 23:34:07 +0200

    faculty member merge

--- src/main/java/com/sis/service/FacultyMemberService.java
+++ src/main/java/com/sis/service/FacultyMemberService.java
@@ -1,61 +1,57 @@
 package com.sis.service;
 
+import com.sis.dao.FacultyMemberRepository;
 import com.sis.dto.FacultyMemberDTO;
+import com.sis.entities.FacultyMember;
 import com.sis.entities.mapper.FacultyMemberMapper;
-import com.sis.entities.mapper.Mapper;
 import com.sis.specification.FacultyMemberSpecification;
 import com.sis.util.PageQueryUtil;
 import com.sis.util.PageResult;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import com.sis.dao.FacultyMemberRepository;
-import com.sis.entities.FacultyMember;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
-public class FacultyMemberService extends BaseServiceImp<FacultyMember>{
-	
-	@Autowired
-	private FacultyMemberRepository facultyMemberRepository;
+@AllArgsConstructor
+public class FacultyMemberService extends BaseServiceImp<FacultyMember> {
 
-	@Autowired
-	private FacultyMemberMapper facultyMemberMapper;
 
-	@Override
-	public JpaRepository<FacultyMember, Long> Repository() {
-		return facultyMemberRepository;
-	}
+    private final FacultyMemberRepository facultyMemberRepository;
+    private final FacultyMemberMapper facultyMemberMapper;
 
-	public void update(FacultyMemberDTO dto){
-		Optional<FacultyMember> object = facultyMemberRepository.findById(dto.getId());
-		if(object.isPresent()){
-			FacultyMember member = facultyMemberMapper.toEntity(dto);
-			facultyMemberRepository.save(member);
-		}
-		else{
-			throw new RuntimeException("faculty member not found");
-		}
-	}
+    @Override
+    public JpaRepository<FacultyMember, Long> Repository() {
+        return facultyMemberRepository;
+    }
 
-	public PageResult<FacultyMemberDTO> search(PageQueryUtil pageUtil, String key){
+    public void update(FacultyMemberDTO dto) {
+        Optional<FacultyMember> object = facultyMemberRepository.findById(dto.getId());
+        if (object.isPresent()) {
+            FacultyMember member = facultyMemberMapper.toEntity(dto);
+            facultyMemberRepository.save(member);
+        } else {
+            throw new RuntimeException("faculty member not found");
+        }
+    }
 
-		Pageable pageable = PageRequest.of(pageUtil.getPage() - 1, pageUtil.getLimit());
+    public PageResult<FacultyMemberDTO> search(PageQueryUtil pageUtil, String key) {
 
-		FacultyMemberSpecification facultyMemberSpecification=new FacultyMemberSpecification(key);
+        Pageable pageable = PageRequest.of(pageUtil.getPage() - 1, pageUtil.getLimit());
 
-		Page<FacultyMember> coursePage = facultyMemberRepository.findAll(facultyMemberSpecification,pageable);
+        FacultyMemberSpecification facultyMemberSpecification = new FacultyMemberSpecification(key);
 
-		PageResult<FacultyMember> pageResult = new PageResult<>(coursePage.getContent(), (int) coursePage.getTotalElements(),
-				pageUtil.getLimit(), pageUtil.getPage());
+        Page<FacultyMember> coursePage = facultyMemberRepository.findAll(facultyMemberSpecification, pageable);
 
-		return facultyMemberMapper.toDataPage(pageResult);
-	}
+        PageResult<FacultyMember> pageResult = new PageResult<>(coursePage.getContent(), (int) coursePage.getTotalElements(),
+                pageUtil.getLimit(), pageUtil.getPage());
+
+        return facultyMemberMapper.toDataPage(pageResult);
+    }
 
 
 }
