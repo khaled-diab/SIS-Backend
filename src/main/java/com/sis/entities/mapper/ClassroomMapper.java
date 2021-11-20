7974d516008c899335349c7ae6a20e3211a6cf96
Moustafa Mohamed <M0D4@GitHub.com>
2021-11-20 19:43:50 +0200

    Add Foreign key Department, Building

--- src/main/java/com/sis/entities/mapper/ClassroomMapper.java
+++ src/main/java/com/sis/entities/mapper/ClassroomMapper.java
@@ -1,6 +1,6 @@
 package com.sis.entities.mapper;
 
-import com.sis.dto.BuildingDTO;
+import com.sis.dto.building.BuildingDTO;
 import com.sis.dto.ClassroomDTO;
 import com.sis.entities.Classroom;
 import com.sis.util.PageResult;
@@ -17,15 +17,18 @@ import static java.util.stream.Collectors.toCollection;
 public class ClassroomMapper implements Mapper<Classroom, ClassroomDTO> {
 
     private final BuildingMapper buildingMapper;
+    private final DepartmentMapper departmentMapper;
 
     @Override
     public ArrayList<ClassroomDTO> toDTOs(Collection<Classroom> entities) {
         return entities.stream().map(this::toDTO).collect(toCollection(ArrayList<ClassroomDTO>::new));
     }
+
     @Override
     public PageResult<ClassroomDTO> toDataPage(PageResult<Classroom> entities) {
         return new PageResult<>(entities.getData().stream().map(this::toDTO).collect(toCollection(ArrayList<ClassroomDTO>::new)), entities.getTotalCount(), entities.getPageSize(), entities.getCurrPage());
     }
+
     @Override
     public ArrayList<Classroom> toEntities(Collection<ClassroomDTO> dtos) {
         return dtos.stream().map(this::toEntity).collect(toCollection(ArrayList<Classroom>::new));
@@ -33,30 +36,28 @@ public class ClassroomMapper implements Mapper<Classroom, ClassroomDTO> {
 
     @Override
     public ClassroomDTO toDTO(Classroom entity) {
-        BuildingDTO buildingDTO = buildingMapper.toDTO(entity.getBuildingId());
-        buildingDTO.setClassroomDTOCollection(null);
-        ClassroomDTO dto=new ClassroomDTO();
+        ClassroomDTO dto = new ClassroomDTO();
         dto.setId(entity.getId());
         dto.setCode(entity.getCode());
-        dto.setName_ar(entity.getName_ar());
-        dto.setName_en(entity.getName_en());
+        dto.setNameAr(entity.getName_ar());
+        dto.setNameEn(entity.getName_en());
         dto.setStatus(entity.getStatus());
         dto.setCapacity(entity.getCapacity());
-        dto.setBuildingDTO(buildingDTO);
-        dto.setDepartmentID(entity.getDepartmentID());
+        dto.setBuildingDTO(buildingMapper.toDTO(entity.getBuildingId()));
+        dto.setDepartmentDTO(departmentMapper.toDTO(entity.getDepartment()));
         return dto;
     }
 
     @Override
     public Classroom toEntity(ClassroomDTO dto) {
-        Classroom entity=new Classroom();
+        Classroom entity = new Classroom();
         entity.setId(dto.getId());
         entity.setCode(dto.getCode());
-        entity.setName_ar(dto.getName_ar());
-        entity.setName_en(dto.getName_en());
+        entity.setName_ar(dto.getNameAr());
+        entity.setName_en(dto.getNameEn());
         entity.setStatus(dto.getStatus());
         entity.setCapacity(dto.getCapacity());
-        entity.setDepartmentID(dto.getDepartmentID());
+        entity.setDepartment(departmentMapper.toEntity(dto.getDepartmentDTO()));
         entity.setBuildingId(buildingMapper.toEntity(dto.getBuildingDTO()));
         return entity;
     }
