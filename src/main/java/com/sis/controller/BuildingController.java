0a39c0b77a0316f64a238fd21550fc4dfda59bb5
Moustafa Mohamed <darsh.m7md@gmail.com>
2021-12-17 02:00:43 +0200

    Add validation

--- src/main/java/com/sis/controller/BuildingController.java
+++ src/main/java/com/sis/controller/BuildingController.java
@@ -6,9 +6,13 @@ import com.sis.entities.mapper.BuildingMapper;
 import com.sis.service.BuildingService;
 import com.sis.util.MessageResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
+@Validated
 @RequestMapping(value = "/api/building")
 @AllArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
@@ -18,7 +22,7 @@ public class BuildingController extends BaseController<Building, BuildingDTO> {
     private final BuildingMapper buildingMapper;
 
     @RequestMapping(value="/addOrUpdate", method = RequestMethod.PUT)
-    public MessageResponse addOrUpdate(@RequestBody BuildingDTO dto) {
+    public MessageResponse addOrUpdate(@Valid @RequestBody BuildingDTO dto) {
         buildingService.save(buildingMapper.toEntity(dto));
         System.out.println("entered");
         return new MessageResponse("Item has been saved successfully");
--- src/main/java/com/sis/dto/ClassroomDTO.java
+++ src/main/java/com/sis/dto/ClassroomDTO.java
@@ -2,17 +2,43 @@ package com.sis.dto;
 
 import com.sis.dto.building.BuildingDTO;
 import com.sis.entities.Department;
+import com.sis.util.Constants;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
+@Validated
 public class ClassroomDTO extends BaseDTO{
+    @NotNull(message = "Required")
+    @NotEmpty(message = "Required")
+    @Pattern(regexp = Constants.ARABIC_CHARACTERS, message = "Arabic Letters only")
     private String nameAr;
+
+    @NotNull(message = "Required")
+    @NotEmpty(message = "Required")
+    @Pattern(regexp = Constants.ENGLISH_CHARACTERS, message = "English Letters only")
     private String nameEn;
+
+    @NotNull(message = "Required")
+    @NotEmpty(message = "Required")
     private String code;
+
     private int status;
+
+    @NotNull(message = "Required")
+    @NotEmpty(message = "Required")
+    @Pattern(regexp = Constants.DIGITS_ONLY, message = "Numerical Digits only")
     private int capacity;
+
+    @NotNull(message = "Required")
     private DepartmentDTO departmentDTO;
+
+    @NotNull(message = "Required")
     private BuildingDTO buildingDTO;
 }
