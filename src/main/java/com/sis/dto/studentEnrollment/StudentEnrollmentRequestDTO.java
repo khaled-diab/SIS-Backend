46b464126f392d137734e6e6822b9631979dcda7
ziadAlymany <ziadmohamed495@gmail.com>
2022-02-14 02:20:10 +0200

    add student enrollment search and filter

--- src/main/java/com/sis/dto/studentEnrollment/StudentEnrollmentRequestDTO.java
+++ src/main/java/com/sis/dto/studentEnrollment/StudentEnrollmentRequestDTO.java
@@ -9,6 +9,12 @@ public class StudentEnrollmentRequestDTO {
     private String searchValue;
     private Long filterCollege;
     private Long filterDepartment;
+    private Long filterAcademicYear;
+    private Long filterAcademicTerm;
+    private Long filterCourse;
+    private Long filterStudyType;
+    private Long filterSectionNumber;
+    private Long filterMajor;
     private String sortDirection;
     private String sortBy;
 
