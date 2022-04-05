14881c8d0b92294c00ef5f0e41fd75272deddd8d
abdoRamadan <abdopro150@gmail.com>
2022-04-06 00:15:54 +0200

    add number of absent student and present

--- src/main/java/com/sis/dto/attendanceReport/FacultyMemberLecturesDTO.java
+++ src/main/java/com/sis/dto/attendanceReport/FacultyMemberLecturesDTO.java
@@ -21,6 +21,8 @@ public class FacultyMemberLecturesDTO extends BaseDTO {
 
     private int absentStudent;
 
+    private double rate ;
+
     private LocalTime lectureStartTime;
 
     private LocalTime lectureEndTime;
