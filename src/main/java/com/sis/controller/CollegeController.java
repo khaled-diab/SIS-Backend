ef5a717979638150cee5093caa2fc3070e3de528
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-06-30 16:14:11 +0200

    fix college code

--- src/main/java/com/sis/controller/CollegeController.java
+++ src/main/java/com/sis/controller/CollegeController.java
@@ -3,7 +3,6 @@ package com.sis.controller;
 import com.sis.dto.college.CollegeDTO;
 import com.sis.dto.college.CollegeRequestDTO;
 import com.sis.entity.College;
-import com.sis.entity.mapper.CollegeMapper;
 import com.sis.service.CollegeService;
 import com.sis.util.MessageResponse;
 import com.sis.util.PageResult;
@@ -16,19 +15,20 @@ import org.springframework.web.bind.annotation.*;
 public class CollegeController extends BaseController<College, CollegeDTO> {
 
     private final CollegeService collegeService;
-    private final CollegeMapper collegeMapper;
-
 
     @PostMapping(value = "/findAll/{page}/{size}")
-    public PageResult<CollegeDTO> findAll(@PathVariable Integer page,
-                                          @PathVariable Integer size,
-                                          @RequestBody CollegeRequestDTO collegeRequestDTO) {
-        return collegeService.getCollegesPage(page,size,collegeRequestDTO);
+    public PageResult<CollegeDTO> findAll(@PathVariable Integer page, @PathVariable Integer size, @RequestBody CollegeRequestDTO collegeRequestDTO) {
+        return collegeService.getCollegesPage(page, size, collegeRequestDTO);
     }
 
     @GetMapping(value = "/deleteCollege/{id}")
     public MessageResponse deleteCollege(@PathVariable(value = "id") Long id) {
         collegeService.deleteById(id);
-        return new MessageResponse("Item has been deleted successfully");
+        return new MessageResponse("College has been deleted successfully");
+    }
+
+    @GetMapping(value = "/checkCollegeCode/{code}")
+    public MessageResponse checkCollegeCode(@PathVariable(value = "code") String code) {
+        return collegeService.checkCollegeCode(code);
     }
 }
