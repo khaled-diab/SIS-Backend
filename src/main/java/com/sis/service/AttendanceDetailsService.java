3954bb65ea65e02c73acb731a6de507796d1c373
abdoRamadan <abdopro150@gmail.com>
2022-07-03 18:01:49 +0200

    add student Attendance Report

--- src/main/java/com/sis/service/AttendanceDetailsService.java
+++ src/main/java/com/sis/service/AttendanceDetailsService.java
@@ -1,18 +1,24 @@
 package com.sis.service;
 
+import com.sis.dto.attendanceDetails.AttendanceBySection;
+import com.sis.dto.attendanceDetails.AttendanceBySectionAndStudentDTO;
 import com.sis.dto.attendanceDetails.AttendanceDetailsDTO;
 import com.sis.dto.attendanceReport.AttendanceReportDTO;
 import com.sis.dto.lecture.LectureDTO;
 import com.sis.dto.student.StudentDTO;
 import com.sis.entity.AttendanceDetails;
+import com.sis.entity.Lecture;
 import com.sis.entity.mapper.AttendanceDetailsMapper;
 import com.sis.repository.AttendanceDetailsRepository;
+import com.sis.repository.LectureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+
+import static java.util.stream.Collectors.toCollection;
 
 @Service
 public class AttendanceDetailsService extends BaseServiceImp<AttendanceDetails>{
@@ -26,6 +32,9 @@ public class AttendanceDetailsService extends BaseServiceImp<AttendanceDetails>{
 
     @Autowired
     private  StudentService studentService;
+
+    @Autowired
+    private LectureRepository lectureRepository;
 
 
     @Override
@@ -52,7 +61,8 @@ public class AttendanceDetailsService extends BaseServiceImp<AttendanceDetails>{
     }
 
     public ArrayList<AttendanceDetailsDTO> getAttendanceDetailsByLecture(long lectureId){
-        ArrayList<AttendanceDetails> attendanceDetails = this.attendanceDetailsRepository.findAttendanceDetailsByLectureId(lectureId);
+        ArrayList<AttendanceDetails> attendanceDetails =
+                this.attendanceDetailsRepository.findAttendanceDetailsByLectureId(lectureId);
         ArrayList<AttendanceDetailsDTO> attendanceDetailsDTOs = new ArrayList<>();
         if(attendanceDetails!= null){
             attendanceDetailsDTOs=  this.attendanceDetailsMapper.toDTOs(attendanceDetails);
@@ -81,7 +91,44 @@ public class AttendanceDetailsService extends BaseServiceImp<AttendanceDetails>{
         }
         this.attendanceDetailsRepository.saveAll(this.attendanceDetailsMapper.toEntities(attendanceDetailsDTOs));
     }
+    //Abdo Ramadan
+    public ArrayList<AttendanceBySectionAndStudentDTO> getAttendanceDetailsBySectoinAndStudentId(Long sectionId,
+                                                                                                 Long studentId){
+        ArrayList<AttendanceDetails> attendanceDetailsList =
+                this.attendanceDetailsRepository.findAttendanceDetailsBySectionIdAndStudentId(sectionId ,studentId);
+        ArrayList<AttendanceBySectionAndStudentDTO> attendanceBySectionAndStudentDTOS = new ArrayList<>();
+        int n = attendanceDetailsList.size();
+        for(int i = 0 ;i < n ;i++){
+            AttendanceDetails attendanceDetails = attendanceDetailsList.get(i);
+            AttendanceBySectionAndStudentDTO attendanceBySectionAndStudentDTO = new AttendanceBySectionAndStudentDTO();
+            attendanceBySectionAndStudentDTO.setAttendanceDate(attendanceDetails.getAttendanceDate());
+            attendanceBySectionAndStudentDTO.setAttendanceStatus(attendanceDetails.getAttendanceStatus());
+            attendanceBySectionAndStudentDTO.setLectureStartTime(attendanceDetails.getLectureStartTime());
+            attendanceBySectionAndStudentDTO.setLectureEndTime(attendanceDetails.getLectureEndTime());
+            attendanceBySectionAndStudentDTOS.add(attendanceBySectionAndStudentDTO);
+        }
+        return  attendanceBySectionAndStudentDTOS;
+    }
 
+    public ArrayList<AttendanceBySection> getAttendanceDetailsBySectoin(Long sectionId  ){
+        ArrayList<AttendanceDetails> attendanceDetailsList =
+                this.attendanceDetailsRepository.findStudentBySectionId(sectionId );
+        ArrayList<AttendanceBySection> attendanceBySectionAndStudentDTOS = new ArrayList<>();
+        int n = attendanceDetailsList.size();
+        ArrayList<Long> lectureIds = lectureRepository.findFacultyMemberLectures(sectionId);
 
+        for(int i = 0 ;i < n ;i++){
+            AttendanceDetails attendanceDetails = attendanceDetailsList.get(i);
+            AttendanceBySection attendanceBySection = new AttendanceBySection();
+            attendanceBySection.setNameOfStudent(attendanceDetails.getStudent().getNameAr());
+            attendanceBySection.setNumberOfLecture(lectureIds.size());
+            Long studentId = attendanceDetails.getStudent().getId();
+            int absenceNumber = attendanceDetailsRepository.findStudentAbsenceLecture(studentId, sectionId).size();
+            attendanceBySection.setIdOfStudent(studentId);
+            attendanceBySection.setAbsentLecture(absenceNumber);
+            attendanceBySectionAndStudentDTOS.add(attendanceBySection);
+        }
+        return  attendanceBySectionAndStudentDTOS;
+    }
 
 }
