06605af9700d3b01717ecb5a1aefe37a163346ed
abdoRamadan <abdopro150@gmail.com>
2022-07-03 18:01:49 +0200

    add student Attendance Report

---
+++ src/main/java/com/sis/dto/attendanceDetails/AttendanceBySection.java
@@ -0,0 +1,12 @@
+package com.sis.dto.attendanceDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class AttendanceBySection {
+    private String nameOfStudent;
+    private int numberOfLecture ;
+    private int absentLecture ;
+    private Long idOfStudent ;
+}
