8223aa79d96e84a9576cc94d87741f9424e6d513
Abdelrahman_Amr <abdozyed2199@gmail.com>
2022-07-05 20:46:53 +0200

    merged with timeTable

--- src/main/java/com/sis/dto/attendanceDetails/AttendanceDetailsDTO.java
+++ src/main/java/com/sis/dto/attendanceDetails/AttendanceDetailsDTO.java
@@ -1,21 +1,9 @@
 package com.sis.dto.attendanceDetails;
 
 import com.sis.dto.BaseDTO;
-import com.sis.dto.course.CourseDTO;
-import com.sis.dto.lecture.LectureDTO;
-import com.sis.dto.section.SectionDTO;
-import com.sis.dto.student.StudentDTO;
-import com.sis.dto.student.StudentRecordDTO;
-import com.sis.util.Constants;
+
 import lombok.*;
 
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import java.time.LocalTime;
-import java.util.Date;
 
 
 @Getter
