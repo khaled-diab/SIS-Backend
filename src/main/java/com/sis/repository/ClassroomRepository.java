f3af9fc3fafe6801db498f755eb9e3bc954e7fc6
ziadAlymany <ziadmohamed495@gmail.com>
2022-07-16 03:35:10 +0200

    fixed filter bugs and timetables bugs

--- src/main/java/com/sis/repository/ClassroomRepository.java
+++ src/main/java/com/sis/repository/ClassroomRepository.java
@@ -2,5 +2,9 @@ package com.sis.repository;
 
 import com.sis.entity.Classroom;
 
+import java.util.List;
+
 public interface ClassroomRepository extends BaseRepository<Classroom> {
+    List<Classroom> getClassroomsByBuildingId(Long buildingId);
+
 }
--- src/main/java/com/sis/service/ClassroomService.java
+++ src/main/java/com/sis/service/ClassroomService.java
@@ -1,6 +1,9 @@
 package com.sis.service;
 
+import com.sis.dto.ClassroomDTO;
+import com.sis.entity.Building;
 import com.sis.entity.Classroom;
+import com.sis.entity.mapper.ClassroomMapper;
 import com.sis.repository.ClassroomSpecification;
 import com.sis.repository.ClassroomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +13,29 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ClassroomService extends BaseServiceImp<Classroom>{
+public class ClassroomService extends BaseServiceImp<Classroom> {
 
-	@Autowired
-	private ClassroomRepository classroomrepository;
+    @Autowired
+    private ClassroomRepository classroomrepository;
 
-	@Override
-	public JpaRepository<Classroom, Long> Repository() {
-		return classroomrepository;
-	}
+    @Autowired
+    private BuildingService buildingService;
 
-	public List<Classroom> search(String key){
-		ClassroomSpecification classroomSpecification = new ClassroomSpecification(key);
-		return classroomrepository.findAll(classroomSpecification);
-	}
+    @Autowired
+    private ClassroomMapper classroomMapper;
+
+    @Override
+    public JpaRepository<Classroom, Long> Repository() {
+        return classroomrepository;
+    }
+
+    public List<Classroom> search(String key) {
+        ClassroomSpecification classroomSpecification = new ClassroomSpecification(key);
+        return classroomrepository.findAll(classroomSpecification);
+    }
+
+    public List<ClassroomDTO> getClassroomsByBuildingId(Long buildingId) {
+        Building building = this.buildingService.findById(buildingId);
+        return this.classroomMapper.toDTOs(this.classroomrepository.getClassroomsByBuildingId(building.getId()));
+    }
 }
