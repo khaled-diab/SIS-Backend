47c124e7fa6335dbc437fd9970c54055c74d8ea3
Abdelrahman_Amr <abdozyed2199@gmail.com>
2022-07-18 02:20:00 +0200

    add current term

--- src/main/java/com/sis/controller/AcademicTermController.java
+++ src/main/java/com/sis/controller/AcademicTermController.java
@@ -2,6 +2,12 @@ package com.sis.controller;
 
 import com.sis.dto.AcademicTermDTO;
 import com.sis.entity.AcademicTerm;
+import com.sis.entity.mapper.AcademicTermMapper;
+import com.sis.service.AcademicTermService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/academicTerms")
 public class AcademicTermController extends BaseController<AcademicTerm, AcademicTermDTO> {
 
+    @Autowired
+    private AcademicTermService academicTermService;
+    @Autowired
+    private AcademicTermMapper academicTermMapper;
+
+    @GetMapping("/getCurrentTerm")
+    public ResponseEntity<AcademicTermDTO> getCurrentTerm(){
+        return new ResponseEntity<>(this.academicTermMapper.toDTO(this.academicTermService.getCurrentAcademicTerm()), HttpStatus.OK);
+    }
 }
